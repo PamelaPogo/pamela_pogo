b552e9f32a8365622a0e8811fafd4928fb726018
PamelaPogo <pme.pogo@yavirac.edu.ec>
2025-10-22 15:53:35 -0500

    Segundo commit

--- prueba_test.py
+++ prueba_test.py
@@ -2,6 +2,6 @@ from calcular import suma
 
 def test_suma():
     assert suma(2,2)==4
-    assert suma(4,4)==7
+    assert suma(4,4)==5
     assert suma(5,5)==6
-    assert suma(6,6)==12
+    assert suma(6,6)==1
